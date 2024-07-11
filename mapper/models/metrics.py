fix: test metric calculation

mapper/models/metrics.py
@@ -27,14 +27,12 @@ class IOU(torchmetrics.Metric):
     def __init__(self, num_classes=3, **kwargs):
         super().__init__(**kwargs)
         self.num_classes = num_classes
-        self.add_state("intersection_observable", default=torch.zeros(
-            num_classes), dist_reduce_fx="sum")
-        self.add_state("union_observable", default=torch.zeros(
-            num_classes), dist_reduce_fx="sum")
-        self.add_state("intersection_non_observable",
-                       default=torch.zeros(num_classes), dist_reduce_fx="sum")
-        self.add_state("union_non_observable", default=torch.zeros(
-            num_classes), dist_reduce_fx="sum")
+        self.add_state("tp_observable", default=torch.zeros(num_classes), dist_reduce_fx="sum")
+        self.add_state("fp_observable", default=torch.zeros(num_classes), dist_reduce_fx="sum")
+        self.add_state("fn_observable", default=torch.zeros(num_classes), dist_reduce_fx="sum")
+        self.add_state("tp_non_observable", default=torch.zeros(num_classes), dist_reduce_fx="sum")
+        self.add_state("fp_non_observable", default=torch.zeros(num_classes), dist_reduce_fx="sum")
+        self.add_state("fn_non_observable", default=torch.zeros(num_classes), dist_reduce_fx="sum")
 
     def update(self, output, data):
 
@@ -55,25 +53,35 @@ class IOU(torchmetrics.Metric):
             gt_mask = gt[..., class_idx]
 
             # For observable areas
-            intersection_observable = torch.logical_and(
-                torch.logical_and(pred_mask, gt_mask), observable_mask
+            gt_mask_bool = gt_mask.bool()
+            tp_observable = torch.logical_and(
+                torch.logical_and(pred_mask, gt_mask_bool), observable_mask
             ).sum()
-            union_observable = torch.logical_and(
-                torch.logical_or(pred_mask, gt_mask), observable_mask
+            fn_observable = torch.logical_and(
+                torch.logical_and(gt_mask_bool, ~pred_mask), observable_mask
             ).sum()
-            self.intersection_observable[class_idx] += intersection_observable
-            self.union_observable[class_idx] += union_observable
-
+            fp_observable = torch.logical_and(
+                torch.logical_and(~gt_mask_bool, pred_mask), observable_mask
+            ).sum()
+            
             # For non-observable areas
-            intersection_non_observable = torch.logical_and(
-                torch.logical_and(pred_mask, gt_mask), non_observable_mask
+            tp_non_observable = torch.logical_and(
+                torch.logical_and(pred_mask, gt_mask_bool), non_observable_mask
             ).sum()
-            union_non_observable = torch.logical_and(
-                torch.logical_or(pred_mask, gt_mask), non_observable_mask
+            fn_non_observable = torch.logical_and(
+                torch.logical_and(gt_mask_bool, ~pred_mask), non_observable_mask
             ).sum()
-
-            self.intersection_non_observable[class_idx] += intersection_non_observable
-            self.union_non_observable[class_idx] += union_non_observable
+            fp_non_observable = torch.logical_and(
+                torch.logical_and(~gt_mask_bool, pred_mask), non_observable_mask
+            ).sum()
+            
+            # Update the state
+            self.tp_observable[class_idx] += tp_observable
+            self.fn_observable[class_idx] += fn_observable
+            self.fp_observable[class_idx] += fp_observable
+            self.tp_non_observable[class_idx] += tp_non_observable
+            self.fn_non_observable[class_idx] += fn_non_observable
+            self.fp_non_observable[class_idx] += fp_non_observable
 
     def compute(self):
         raise NotImplemented
@@ -85,28 +93,36 @@ class ObservableIOU(IOU):
         self.class_idx = class_idx
 
     def compute(self):
-        return (self.intersection_observable / (self.union_observable + 1e-6))[self.class_idx]
-
-
+        # return (self.intersection_observable / (self.union_observable + 1e-6))[self.class_idx]
+        intersection_observable = self.tp_observable[self.class_idx]
+        union_observable = self.tp_observable[self.class_idx] + self.fn_observable[self.class_idx] + self.fp_observable[self.class_idx]
+        return intersection_observable / (union_observable + 1e-6)
+    
 class UnobservableIOU(IOU):
     def __init__(self, class_idx=0, **kwargs):
         super().__init__(**kwargs)
         self.class_idx = class_idx
 
     def compute(self):
-        return (self.intersection_non_observable / (self.union_non_observable + 1e-6))[self.class_idx]
-
-
+        # return (self.intersection_non_observable / (self.union_non_observable + 1e-6))[self.class_idx]
+        intersection_non_observable = self.tp_non_observable[self.class_idx]
+        union_non_observable = self.tp_non_observable[self.class_idx] + self.fn_non_observable[self.class_idx] + self.fp_non_observable[self.class_idx]
+        return intersection_non_observable / (union_non_observable + 1e-6)
+    
 class MeanObservableIOU(IOU):
     def compute(self):
-        return self.intersection_observable.sum() / (self.union_observable.sum() + 1e-6)
-
+        # return self.intersection_observable.sum() / (self.union_observable.sum() + 1e-6)
+        intersection_observable = self.tp_observable.sum()
+        union_observable = self.tp_observable.sum() + self.fn_observable.sum() + self.fp_observable.sum()
+        return intersection_observable / (union_observable + 1e-6)
 
 class MeanUnobservableIOU(IOU):
     def compute(self):
-        return self.intersection_non_observable.sum() / (self.union_non_observable.sum() + 1e-6)
-
-
+        # return self.intersection_non_observable.sum() / (self.union_non_observable.sum() + 1e-6)
+        intersection_non_observable = self.tp_non_observable.sum()
+        union_non_observable = self.tp_non_observable.sum() + self.fn_non_observable.sum() + self.fp_non_observable.sum()
+        return intersection_non_observable / (union_non_observable + 1e-6)
+    
 class mAP(torchmetrics.classification.MultilabelPrecision):
     def __init__(self, num_labels, **kwargs):
         super().__init__(num_labels=num_labels, **kwargs)